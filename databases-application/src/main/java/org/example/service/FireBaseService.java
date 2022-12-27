8ac31ac7ec91586391ce368647709f41f77666c1
Adam Wadowski <adamwadowski2@gmail.com>
2022-12-27 23:32:46 +0100

    podejście pod baze danych

---
+++ databases-application/src/main/java/org/example/service/FireBaseService.java
@@ -0,0 +1,38 @@
+package org.example.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FireBaseService {
+
+    FirebaseDatabase db;
+
+    public FireBaseService() throws IOException {
+        File file = new File(
+                getClass().getClassLoader().getResource("key.json").getFile()
+        );
+
+        FileInputStream fis = new FileInputStream(file);
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(fis))
+                .setDatabaseUrl("https://console.firebase.google.com/u/0/project/databases-4370c")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        db = FirebaseDatabase.getInstance();
+    }
+
+    public FirebaseDatabase getDb() {
+        return db;
+    }
+
+}
+
