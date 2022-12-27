66173ff6d8d4c9df264a6a3fe3d513c930df783d
Adam Wadowski <adamwadowski2@gmail.com>
2022-12-27 20:20:56 +0100

    poprawki podstawowe

--- databases-application/src/main/java/org/example/Main.java
+++ databases-application/src/main/java/org/example/Main.java
@@ -2,17 +2,9 @@ package org.example;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
-@EnableCaching
-@EnableScheduling
-@EnableAsync
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
